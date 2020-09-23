209c97228c468505380538825fd5d62f6cd5638d
Atilin <71184528+Atilin@users.noreply.github.com>
2020-09-23 17:35:03 +0300

    Update 9.cpp
    Homework

--- 9.cpp
+++ 9.cpp
@@ -1,13 +1,12 @@
 //Посчитать 2^n - 1 с помощью побитовых сдвигов и инверсии
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
 	int n;
 	cin >> n;
 	n = 1 << n;
-	n *= -1;
+	n = -n;
 	n = ~n;
 	cout << n;
 	return 0;
