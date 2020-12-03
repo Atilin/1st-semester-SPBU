2adc69b34c79037fd5e9bb0619d87af4e3d97db7
Atilin <71184528+Atilin@users.noreply.github.com>
2020-12-03 15:49:23 +0300

    aaaaaaaaaaa
    aaaaaaaaaa

--- aaaaaaaaaaaaaaa/aaaaaaaaaaaaaaa/Source.cpp
+++ aaaaaaaaaaaaaaa/aaaaaaaaaaaaaaa/Source.cpp
@@ -7,8 +7,10 @@ using namespace std;
 double myPow(double k, int n) // для неотрицательной степени
 {
 	double ans = k;
+
 	if (n == 0)
 		return 1;
+
 	for (int i = 1; i < n; ++i)
 	{
 		ans *= k;
@@ -16,49 +18,32 @@ double myPow(double k, int n) // для неотрицательной степени
 	return ans;
 }
 
-double teylor(double x, int k)
-{
-	double sum = 0;
-	for (int i = 0; i <= k; ++i)
-	{
-		sum = sum + myPow(-1, i) * myPow(x, 2 * i + 1) / (2 * i + 1);
-	}
-	return sum;
-}
-
 double myFunction(double x, int n)
 {
 	int m = 0;
-	double nznak1 = teylor(x, m) * myPow(10, n);
-	double nznak2 = teylor(x, m+1) * myPow(10, n);
+	int aa1 = -1;
+	int aa2 = -2;
+	double ans = 0;
 
-	//cout << "teylor " << teylor(x, m) << endl;
-
-	//cout << "myPow " << myPow(10, n) << endl;
-
-	//cout << "nznak " << nznak1 << ' ' << nznak2 << endl;
-
-	int aa1 = nznak1;
-	int aa2 = nznak2;
-	//cout <<"aa "<< aa1 << ' ' << aa2 << endl;
 	while (aa1 != aa2)
 	{
-		//teylor(x, m);
-		//teylor(x, m + 1);
-
-		nznak1 = teylor(x, m) * myPow(10, n);
-		nznak2 = teylor(x, m+1) * myPow(10, n);
-
-		aa1 = nznak1;
-		aa2 = nznak2;
-
-		//cout << "aa1" << ' ' << aa1 << endl;
-
-		//cout << "aa2" << ' ' << aa2 << endl;
+		double sum1 = 0;
+		double sum2 = 0;
+		for (int i = 0; i <= m + 1; ++i)
+		{
+			sum2 = sum2 + myPow(-1, i) * myPow(x, 2 * i + 1) / (2 * i + 1);
+			if (i == m)
+			{
+				sum1 = sum2;
+				ans = sum1;
+			}
+		}
+		aa1 = sum1 * myPow(10, n);
+		aa2 = sum2 * myPow(10, n);
 
 		m++;
 	}
-	return teylor(x, m - 1);
+	return ans;
 }
 
 int main()
@@ -67,6 +52,7 @@ int main()
 	int n = 0;
 
 	cin >> x >> n;
+
 	if (x >= 1 || x <= -1)
 	{
 		cout << "wrong input\n";
@@ -77,9 +63,6 @@ int main()
 		cout << "wrong input\n";
 		return 0;
 	}
-	//cout << myPow(-2, 2);
-
-	//cout <<"teylor "<< teylor(x, n) << endl;
 
 	cout << setprecision(9) << atan(x) << endl;
 	cout << setprecision(9) << myFunction(x, n) << endl;
